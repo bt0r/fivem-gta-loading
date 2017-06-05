682a998470df640e58f89483002c94824a35f43b
Thibaut <the.btor@gmail.com>
2017-06-05 14:20:11 +0200

    First commit

---
+++ __resource.lua
@@ -0,0 +1,42 @@
+--
+-- Author bt0r
+-- Twitch : bt0r
+-- Twitter : biiitor
+--
+files {
+    'files/loader.html',
+    'files/js/loader.js',
+    'files/css/loader.css',
+    'files/sound/intro.ogg',
+    'files/img/background/wall1.png',
+    'files/img/background/wall2.png',
+    'files/img/background/wall3.png',
+    'files/img/background/wall4.png',
+    'files/img/background/wall5.png',
+    'files/img/background/wall6.png',
+    'files/img/background/wall7.png',
+    'files/img/background/wall8.png',
+    'files/img/background/wall9.png',
+    'files/img/background/wall10.png',
+    'files/img/background/wall11.png',
+    'files/img/background/wall12.png',
+    'files/img/background/wall13.png',
+    'files/img/characters/1.png',
+    'files/img/characters/2.png',
+    'files/img/characters/3.png',
+    'files/img/characters/4.png',
+    'files/img/characters/5.png',
+    'files/img/characters/6.png',
+    'files/img/characters/7.png',
+    'files/img/characters/8.png',
+    'files/img/characters/9.png',
+    'files/img/characters/10.png',
+    'files/img/characters/11.png',
+    'files/img/characters/12.png',
+    'files/img/characters/13.png',
+    'files/img/characters/14.png',
+    'files/img/characters/15.png',
+    'files/img/logo.png',
+}
+loadscreen 'files/loader.html'
+
---
+++ files/js/loader.js
@@ -0,0 +1,79 @@
+function Loader(){
+    this.loader         = $('#loading_box');
+    this.background     = $('#loadingBackground');
+    this.character      = $('#loadingCharacter');
+    this.maxCharacters  = 15;
+    this.maxBackgrounds = 8;
+    this.animationTime  = 7000;
+
+    this.load = function(){
+        this.loader.show();
+    }
+
+    this.randomImages = function(){
+        var random                  = 0;
+        var previousRandom          = 0;
+        var previousRandomCharacter = 0;
+        var randomCharacter         = 0;
+
+        while (previousRandom == random || randomCharacter == previousRandomCharacter) {
+            random = Math.round(Math.random() * this.maxBackgrounds - 1) + 1;
+            randomCharacter = Math.round(Math.random() * this.maxCharacters - 1) + 1;
+        }
+
+        // Change images
+        previousRandom = random;
+        previousRandomCharacter = randomCharacter;
+        var backgroundUrl = "img/background/wall" + random + ".png";
+        var characterUrl  = "img/characters/" + randomCharacter + ".png";
+
+        return [backgroundUrl,characterUrl];
+    }
+
+    this.nextAnimation = function (isFirst) {
+        var _this         = this;
+        var rightToLeft   = Math.round(Math.random() * 1) == 1 ? true : false;
+        var randomImages  = this.randomImages();
+        var backgroundUrl = randomImages[0];
+        var characterUrl  = randomImages[1];
+
+        if(!isFirst){
+            // Fade out images and after change it
+            this.background.fadeOut(2000, function () {
+                _this.background.attr('src', backgroundUrl);
+                _this.background.fadeIn(2000);
+
+            });
+            this.character.fadeOut(2000, function () {
+                _this.character.attr('src', characterUrl);
+                _this.character.fadeIn(2000);
+            });
+        }else{
+            //Show images directly
+            _this.background.attr('src', backgroundUrl);
+            _this.character.attr('src', characterUrl);
+        }
+
+        // Alternate right/left directions
+        if (rightToLeft) {
+            window.setTimeout(function () {
+                _this.background.removeClass().addClass("rightToLeftBG");
+                _this.character.removeClass().addClass("leftToRight");
+            }, 2000);
+        } else {
+            window.setTimeout(function () {
+                _this.background.removeClass().addClass("leftToRightBG");
+                _this.character.removeClass().addClass("rightToLeft");
+            }, 2000);
+        }
+    }
+
+    this.open = function () {
+        this.nextAnimation(true);
+        var _this = this;
+        window.setInterval(function () {
+            _this.nextAnimation(false);
+        }, this.animationTime);
+    }
+
+}
